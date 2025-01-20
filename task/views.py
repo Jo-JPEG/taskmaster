f60c8113f788b74ad85acfb223879f992c63a773
Jo <joanne.c.wright@googlemail.com>
2025-01-20 16:43:41 +0000

    add tables for to do and done items

--- task/views.py
+++ task/views.py
@@ -4,9 +4,6 @@ from .models import Task
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-
-def uncompleted_tasks(request):
-    tasks = Task.objects.filter(completed=False).order_by('due_date')
-    return render(request, 'uncompleted_tasks.html', {'tasks': tasks})
-
+    uncompleted_tasks = Task.objects.filter(completed=False).order_by('due_date')
+    completed_tasks = Task.objects.filter(completed=True).order_by('due_date')
+    return render(request, 'index.html', {'uncompleted_tasks': uncompleted_tasks, 'completed_tasks': completed_tasks})
